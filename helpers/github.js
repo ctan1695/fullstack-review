e6fcce07bccc216c564448848e30270e4e24807c
Chloe Tan <chloe.tan95@gmail.com>
2021-02-13 14:55:59 -0500

    Added github api token as env variable and updated github helper to use this variable instead of config when in production

--- helpers/github.js
+++ helpers/github.js
@@ -1,18 +1,17 @@
 const axios = require('axios');
-const config = require('../config.js');
+const config;
+
+//Only look for config file if it's not production!
+if (process.env.NODE_ENV !== 'production') {
+  config = require('../config.js');
+}
 
 let getReposByUsername = (username) => {
-
-  // TODO - Use the axios module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out,
-  // but you'll have to fill in the URL
   let options = {
     url: 'https://api.github.com/users/' + username.toString(),
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${process.env.GITHUB_API_TOKEN || config.TOKEN}`
     }
   };
 
