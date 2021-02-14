45d99dd086b9ffe4bd591e505f82a1c81c6da6b7
Chloe Tan <chloe.tan95@gmail.com>
2021-02-14 09:43:22 -0500

    Redid node modules

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
+    rules : [
       {
         test : /\.jsx?/,
         include : SRC_DIR,
-        loader : 'babel-loader',      
+        loader : 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
